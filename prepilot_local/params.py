2bca422b57b2efd7085fb5ae120ade118e8bfc07
Egor Shishkovets <egorshishkovets@MacBook-Air-Egor.local>
2022-05-17 10:55:23 +0300

    ab init

---
+++ prepilot_local/params.py
@@ -0,0 +1,41 @@
+from typing import Dict, List, Any, Union, Optional, Callable, Tuple
+from pydantic import Field, root_validator
+from pydantic.dataclasses import dataclass
+import numpy as np
+
+
+@dataclass
+class PrepilotParams:
+    """Prepilot experiment parameters class.
+
+    Args:
+        metrics_names: metrics which will be compare in experiments.
+        injects: injects represent MDE values.
+        min_group_size: minimal value of groups sizes
+        max_group_size: maximal value of groups sizes
+        step: Spacing between min_group_size and max_group_size
+        iterations_number: count of splits for each element in group_sizes
+        max_beta_score: max level of II type error
+        min_beta_score: min level of II type error, that will be calculated
+        for greater group size, if it has been found on earlier
+        and data haven't any changes turn to True otherwise False
+    """
+    metrics_names: List[str]
+    injects: List[float]
+    min_group_size: int
+    max_group_size: int
+    step: int
+    bootstrap_metric: Optional[Callable[[Any], float]] = np.mean
+    iterations_number: int = 10
+    n_buckets: int = 1000
+    max_beta_score: float = 0.2
+    min_beta_score: float = 0.05
+
+    @root_validator
+    @classmethod
+    def groups_sizes_validator(cls, values):
+        min_group_size = values.get("min_group_size")
+        max_group_size = values.get("max_group_size")
+        assert max_group_size > min_group_size, \
+            "max_group_size should be more than min_group_size"
+        return values
